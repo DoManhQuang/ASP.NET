4dfbdb5a67ca682d65e39823eb2a72eaa7ad5680
md_quangdm <quangdm@ghtk.vn>
2019-11-25 11:55:55 +0700

    create user default

--- WebBTLNhom8/MasterPage01.master.cs
+++ WebBTLNhom8/MasterPage01.master.cs
@@ -9,6 +9,7 @@ public partial class MasterPage01 : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        lnkbtnDonhang.Visible = false;
         lblSoluongmua.Text = "";
         List<InfoThucPham> lstThucpham = (List<InfoThucPham>)Session["cart"];
         if (Session["cart"] != null)
@@ -17,6 +18,28 @@ public partial class MasterPage01 : System.Web.UI.MasterPage
         }
         else {
             lblSoluongmua.Text = "0";
+        }
+
+
+        // test
+        if(Session["users"] != null)
+        {
+            lnkbtnDonhang.Visible = true;
+        }
+        if (Session["users"] == null)
+        {
+            Quang_Khachhang khachhangtest = new Quang_Khachhang("1", "Quang", "123456", "quangdm@ghtk.vn");
+            Session["users"] = khachhangtest;
+            lnkbtnDonhang.Visible = true;
+        }
+    }
+
+    protected void lnkbtnDonhang_Click(object sender, EventArgs e)
+    {
+        if (Session["users"] != null)
+        {
+            Quang_Khachhang khachhang = (Quang_Khachhang)Session["users"];
+            Response.Redirect("users_Donhang.aspx?MaTK=" + khachhang.getMaKH());
         }
     }
 }
